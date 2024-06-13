a391ba08a53fdf59f860cecd41427019ceaf12bd
unknown <amirboulahrouz1@gmail.com>
2024-06-13 18:19:46 -0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <header className="h-[200px] text-white flex items-center justify-center"> 
+    <header className=" h-[200px] text-white flex items-center justify-center"> 
       <a href="/"> 
     <h1 className="text-3xl text-black"> IMAGE<span className="text-blue-400">DESCRIPTOR </span>  🤖</h1>
    </a> 
