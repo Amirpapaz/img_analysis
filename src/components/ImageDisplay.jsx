4dfeccaa842ea9680f351899e9374087579c5680
Amirpapaz <amirboulahrouz1@gmail.com>
2024-06-16 19:33:50 -0400

    Regenerate + .env

--- src/components/ImageDisplay.jsx
+++ src/components/ImageDisplay.jsx
@@ -5,12 +5,13 @@ export default function ImageDisplay(props) {
     const {file, url, resetButton} = props
     const [result, setResult] = useState(null)
     const [loading, setLoading] = useState(true)
-
+    const [regenerate,setRegenerate] = useState(false)
+  
     useEffect(() => {
       async function fetchData() {
         if (file || url) {
           try {
-            const captionResult = await getImageCaption(file, url)
+            const captionResult = await getImageCaption(file, url,false)
             setResult(captionResult)
             setLoading(false)
           } catch (error) {
@@ -23,19 +24,38 @@ export default function ImageDisplay(props) {
       fetchData()
      
     }, [file,url])
-
+    
+    function Regenerate(){
+      setRegenerate(true)
+      setLoading(true)
+      async function Regenerate() {
+        if (file || url) {
+          try {
+            const captionResult = await getImageCaption(file, url,true)
+            setResult(captionResult)
+            setLoading(false)
+          } catch (error) {
+            console.error("Error fetching caption:", error)
+            setResult("Failed to fetch caption")
+            
+          }
+        } 
+      }
+    Regenerate()
+    setRegenerate(false) //clear Regenerate value
+  }
   return (
     <>
     <div className="flex flex-col justify-center items-center w-full h-auto p-2 "> 
     { file ? <img className=" max-w-[300px] justify-center p-2 " src={`${file}`}/> : <img className=" max-w-[300px] justify-center p-2" src={`${url}`}/> }
    
-   {loading ? (<i className="py-2 fa-solid fa-gear loadingState "></i>) : (<p className="flex specialBtn px-4 py-2 font-bold rounded-xl items-center text-base justify-between gap-4 mx-auto  max-w-full my-4 specialBtn"> {result} </p>)}
+   {loading ? (<i className="fa-solid fa-spinner loadingState"></i>) : (<p className="flex specialBtn px-4 py-2 font-bold rounded-xl items-center text-base justify-between gap-4 mx-auto  max-w-full my-4 specialBtn"> {result} </p>)}
 
    <div className="flex px-4 gap-4"> 
     <button className=" hover:scale-105 flex specialBtn px-4 py-2 font-bold rounded-xl items-center text-base justify-between gap-4 mx-auto  max-w-full my-4 specialBtn" onClick={resetButton}> 
       Reset
     </button>
-    <button className=" hover:scale-105 flex specialBtn px-4 py-2 font-bold rounded-xl items-center text-base justify-between gap-4 mx-auto  max-w-full my-4 specialBtn"> 
+    <button onClick={Regenerate} className=" hover:scale-105 flex specialBtn px-4 py-2 font-bold rounded-xl items-center text-base justify-between gap-4 mx-auto  max-w-full my-4 specialBtn"> 
       Regenerate text
     </button>
     </div>
--- src/utils/huggingface.js
+++ src/utils/huggingface.js
@@ -1,10 +1,11 @@
 import { HfInference } from "@huggingface/inference"
 
-const HF_ACCESS_TOKEN = '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'
+const HF_ACCESS_TOKEN = `${import.meta.env.VITE_API_URL}`
 const inference = new HfInference(HF_ACCESS_TOKEN)
-const model = "Salesforce/blip-image-captioning-large"
+let modelTracker = 0
+const models = ["dblasko/blip-dalle3-img2prompt","Salesforce/blip-image-captioning-large","microsoft/trocr-base-handwritten"]
 
-export async function getImageCaption(file, url) {
+export async function getImageCaption(file, url,regenerate) {
   try {
     const imgUrl = file || url
     console.log("Fetching image from:", imgUrl)
@@ -17,9 +18,16 @@ export async function getImageCaption(file, url) {
     const imageBlob = await response.blob()
     console.log("Image blob fetched:", imageBlob)
 
+    if(regenerate === true){
+      modelTracker = modelTracker + 1   
+
+      if(modelTracker === models.length) {
+        modelTracker = 0
+      }
+    }
     const result = await inference.imageToText({
       data: imageBlob,
-      model: model,
+      model: models[modelTracker],
     })
     console.log("Inference result:", result)
 
