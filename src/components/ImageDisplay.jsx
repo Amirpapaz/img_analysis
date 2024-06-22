ae0ef6f6b37799f8002c272480943043ece4b1ba
unknown <amirboulahrouz1@gmail.com>
2024-06-22 09:51:43 -0400

    updated image look

--- src/components/ImageDisplay.jsx
+++ src/components/ImageDisplay.jsx
@@ -48,9 +48,10 @@ export default function ImageDisplay(props) {
   return (
     <>
     <div className="flex flex-col justify-center items-center w-full h-auto p-2 "> 
+      <div className=" object-cover shadow"> 
     { file ? <img className=" max-w-[300px] justify-center p-2 " src={`${file}`}/> : <img className=" max-w-[300px] justify-center p-2" src={`${url}`}/> }
-   
-   {loading ? (<i className="fa-solid fa-spinner loadingState"></i>) : (<p className="flex specialBtn px-4 py-2 font-bold rounded-xl items-center text-base justify-between gap-4 mx-auto  max-w-full my-4 specialBtn"> {result} </p>)}
+   </div>
+   {loading ? (<i className="fa-solid fa-spinner loadingState p-4 "></i>) : (<p className="flex specialBtn px-4 py-2 font-bold rounded-xl items-center text-base justify-between gap-4 mx-auto  max-w-full my-4 specialBtn"> {result} </p>)}
 
    <div className="flex px-4 gap-4"> 
     <button className=" hover:scale-105 flex specialBtn px-4 py-2 font-bold rounded-xl items-center text-base justify-between gap-4 mx-auto  max-w-full my-4 specialBtn" onClick={resetButton}> 
